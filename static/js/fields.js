c3a19bf9a6677c2d5918b18186733d4fa1f40118
Elio OUEDRAOGO <elio.ouedraogo@etu.unistra.fr>
2025-06-09 00:38:49 +0200

    Modified insertion system for fields

---
+++ static/js/fields.js
@@ -0,0 +1,97 @@
+let holdInterval;
+var table = document.getElementById("table");
+
+document.getElementById("entry").addEventListener("click", row_selection);
+
+document.querySelectorAll(".input").forEach((element) => {
+  element.addEventListener("blur", () => {
+    element.value = rectified_value(
+      Number(element.value.trim()),
+      Number(element.parentElement.dataset.min.trim()),
+      Number(element.parentElement.dataset.max.trim()),
+    );
+  });
+});
+
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "Enter":
+      var activeElement = document.activeElement;
+      if (
+        activeElement.tagName == "INPUT" &&
+        activeElement.parentElement.classList.contains("numbers_entry_div")
+      ) {
+        activeElement.value = rectified_value(
+          Number(activeElement.value.trim()),
+          Number(activeElement.parentElement.dataset.min.trim()),
+          Number(activeElement.parentElement.dataset.max.trim()),
+        );
+        return;
+      }
+      if (!is_row_selected()) insert();
+      else {
+        // Element clicked is name input of row that is not entry
+        if (emptyName()) sendForbidLaunch();
+        else sendAllowLaunch();
+      }
+      activeElement.blur();
+      return;
+    case "Escape":
+      if (is_row_selected()) unselect_all();
+      document.activeElement.blur();
+      return;
+    case "Delete":
+      if (is_row_selected()) get_selected_row().remove();
+    default:
+      return;
+  }
+});
+
+function emptyName() {
+  const exists = Array.from(table.rows).some((row) => {
+    if (row.querySelector(".field_id") && row.id != "entry")
+      return row.querySelector(".field_id").value.trim() == "";
+  });
+  return exists;
+}
+
+function insert() {
+  // Insertion control
+  var row = document.getElementById("entry");
+  if (row.querySelector(".field_id").value == "") return;
+
+  // Clone setup
+
+  const clone = row.cloneNode(true);
+  clone.id = "";
+  clone.querySelector(".field_id").readOnly = true;
+  clone.cells[row.cells.length - 1].innerHTML =
+    '<a href="#" onclick="openPopup(this); return false;">Click here</a>';
+
+  // Copying selects
+
+  var clone_copy = clone.querySelectorAll("select");
+  var row_copy = row.querySelectorAll("select");
+
+  for (var i = 0; i < clone_copy.length; i++) {
+    clone_copy[i].value = row_copy[i].value;
+  }
+
+  // Cloning listeners
+
+  clone.addEventListener("click", row_selection);
+  clone.querySelector(".field_id").addEventListener("blur", () => {
+    if (emptyName()) sendForbidLaunch();
+    else sendAllowLaunch();
+  });
+
+  table.tBodies[0].insertBefore(clone, row);
+
+  // Cleaning input
+  clean_input(row);
+}
+
+function clean_input(row) {
+  row.querySelector(".field_id").value = "";
+  row.querySelector(".field_portion").value = "Terrain entier";
+}
