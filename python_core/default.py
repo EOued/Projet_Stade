80c37b194bb55beca798557ac44417b3416e4fce
Elio OUEDRAOGO <elio.oued.work@gmail.com>
2025-08-08 18:08:00 +0200

    Added files

---
+++ python_core/default.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+GAMEFORMAT = ["Whole Field", "Half Field", "Quarter Field"]
+FIELDTYPE = ["Natural Field", "Synthetic Field"]
+
+SETTINGS_FILE = "settings.json"
+TEAMS_FILE = "teams.json"
+FIELDS_FILE = "fields2.json"
+
+DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+
+class FieldPortion(Enum):
+    WHOLE = 0
+    HALF = 1
+    QUARTER = 2
+
+class FieldType(Enum):
+    NATURAL = 0
+    SYNTHETIC = 1
+    QUARTER = 2
